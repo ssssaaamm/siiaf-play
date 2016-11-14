065ec8e8ed725bb748a073ffdbe3e52f6906adf5
RuthTorrentoLara <rstorrento.25@gmail.com>
2016-11-14 02:30:07 -0600

    mascaras jquery, revisar caracteres opcionales de km y placa en /masked.js

---
+++ public/javascripts/masked.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.dui').mask('00000000-0');
+  $('.nit').mask('0000-000000-000-0');
+  $('.licencia').mask('0000-000000-000-0');
+  $('.telefono').mask('(000) 0000-0000');
+  $('.placa').mask('C 0-000');
+  $('.periodo').mask('00');
+  $('.km').mask('00???.??', {optional: true});
+ });
